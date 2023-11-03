7d9a1f72b50543f4bce373df6dd72f9f211397ac
MHShihan <mahmodul.no.1@gmail.com>
2023-11-04 00:27:37 +0600

    .env and .gitignore file has been created to secure MongoDB

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//mahmodulno1
-//9FSbIaGD79eoj7Ki
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-  "mongodb+srv://mahmodulno1:9FSbIaGD79eoj7Ki@cluster0.88ffpvi.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.88ffpvi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
